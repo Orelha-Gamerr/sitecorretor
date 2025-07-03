e78b177e3e2f7d0f7f6011be458063e3866211f3
Orelha-Gamerr <angelolucietto@gmail.com>
2025-07-03 07:40:09 -0300

    DATABASE UPDATE

---
+++ site/admin/database/db_pweb1_2025.sql
@@ -0,0 +1,88 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           8.0.30 - MySQL Community Server - GPL
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para bancoimoveis
+CREATE DATABASE IF NOT EXISTS `bancoimoveis` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bancoimoveis`;
+
+-- Copiando estrutura para tabela bancoimoveis.bairro
+CREATE TABLE IF NOT EXISTS `bairro` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela bancoimoveis.bairro: ~0 rows (aproximadamente)
+INSERT INTO `bairro` (`id`, `nome`) VALUES
+	(1, 'Centro'),
+	(2, 'Jardim Itália');
+
+-- Copiando estrutura para tabela bancoimoveis.imagens
+CREATE TABLE IF NOT EXISTS `imagens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `idImovel` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela bancoimoveis.imagens: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela bancoimoveis.imovel
+CREATE TABLE IF NOT EXISTS `imovel` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `cliente` varchar(150) DEFAULT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  `valor` int DEFAULT NULL,
+  `area` int DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `idBairro` int DEFAULT NULL,
+  `rua` varchar(100) DEFAULT NULL,
+  `cep` varchar(20) DEFAULT NULL,
+  `quartos` int DEFAULT NULL,
+  `banheiros` int DEFAULT NULL,
+  `vagas` int DEFAULT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `capa` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela bancoimoveis.imovel: ~0 rows (aproximadamente)
+INSERT INTO `imovel` (`id`, `nome`, `cliente`, `descricao`, `valor`, `area`, `cidade`, `estado`, `idBairro`, `rua`, `cep`, `quartos`, `banheiros`, `vagas`, `tipo`, `status`, `capa`) VALUES
+	(1, 'CASA', 'Leandro Lucietto', 'Apartamento domiciliar', 700000, 45, 'Chapecó', 'Santa Catarina', 1, 'Osvaldo Cruz', '12345-09', 2, 3, 2, 'Apartamento', 'Em negociação', 'teste');
+
+-- Copiando estrutura para tabela bancoimoveis.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `senha` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `login` varchar(80) DEFAULT NULL,
+  `cargo` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Copiando dados para a tabela bancoimoveis.usuario: ~0 rows (aproximadamente)
+INSERT INTO `usuario` (`id`, `nome`, `senha`, `login`, `cargo`) VALUES
+	(1, 'Angelo Antonio Lucietto', '$2y$10$FmithcIg7QW9x05f5jQJu.qTwkO6.WhrXkIW0WZXVe/AAQk8pMTVu', NULL, 'administrador');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
